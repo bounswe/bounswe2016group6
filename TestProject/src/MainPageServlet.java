d0f45af5972f121d76b48df3d4e567292ef6c4a1
ahmet zorer <ahmetcjr@hotmail.com>
2016-04-25 17:40:52 +0300

    Main page address is fixed

--- TestProject/src/MainPageServlet.java
+++ TestProject/src/MainPageServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MainPageServlet
  */
-@WebServlet("/MainPageServlet")
+@WebServlet("/")
 public class MainPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
