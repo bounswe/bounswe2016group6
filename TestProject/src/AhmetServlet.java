7e93cc3c91fc631cbb316aedf178b2a6a663fc20
ahmet zorer <ahmetcjr@hotmail.com>
2016-05-02 23:24:21 +0300

    query is get

--- TestProject/src/AhmetServlet.java
+++ TestProject/src/AhmetServlet.java
@@ -1,6 +1,13 @@
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,16 +33,28 @@ public class AhmetServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Ahmet Zorer");
+		PrintWriter out = response.getWriter();
+		String query = "SELECT ?item ?itemLabel ?pic WHERE { ?item ?x1 wd:Q146 . OPTIONAL { ?item wdt:P18 ?pic } SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } }";
+        String website = "https://query.wikidata.org/sparql?query=";
+		try {
+			String queryResult = getHTML("https://query.wikidata.org/sparql?query=SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}");
+			out.println(queryResult);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public static String getHTML(String urlToRead) throws Exception {
+		 URL oracle = new URL(urlToRead);
+	        BufferedReader in = new BufferedReader(
+	        new InputStreamReader(oracle.openStream()));
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	        String sum,inputLine;
+	        while ((inputLine = in.readLine()) != null)
+	            System.out.println(inputLine);
+	        in.close();
+	      return inputLine;
+	   }
 
 }
