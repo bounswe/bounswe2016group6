21b7b939a4763d2a92bb44f27363c833ca0b790f
eozd <esref.ozdemir@boun.edu.tr>
2016-05-08 21:16:01 +0300

    adds javascript file for home page.
    javascript file sends requests with certain request id's. Servlet acts
    accordingly with respect to the given request type.

--- TestProject/src/EsrefServlet.java
+++ TestProject/src/EsrefServlet.java
@@ -24,6 +24,7 @@ import java.util.regex.*;
 @WebServlet("/esref_ozdemir")
 public class EsrefServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String jsonData = null;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -37,28 +38,47 @@ public class EsrefServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String s1 = 
+		String requestType = request.getParameter("type");
+		if (requestType == null) {
+			request.getRequestDispatcher("/WEB-INF/esrefHome.jsp").forward(request, response);;
+		} else if (requestType.equals("queryData")) {
+			String inputTerm = request.getParameter("input");
+			String s1 = 
 					"PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
 					"PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
 					"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
 					"PREFIX wd: <http://www.wikidata.org/entity/>\n" +
 					"select ?objectLabel ?countryLabel ?coord\n" +
 					"where {\n" +
-						"?object wdt:P31 wd:Q46169 . \n" +
-						"?object wdt:P625 ?coord .\n" +
-						  "?object wdt:P17 ?country .\n" +
-						"SERVICE wikibase:label {\n" +
-							"bd:serviceParam wikibase:language \"en\" .\n" +
-						"}\n" +
+					"?object wdt:P31 wd:Q46169 . \n" +
+					"?object wdt:P625 ?coord .\n" +
+					"?object wdt:P17 ?country .\n" +
+					"SERVICE wikibase:label {\n" +
+					"bd:serviceParam wikibase:language \"en\" .\n" +
+					"}\n" +
 					"}" +
 					"LIMIT 300\n";
-		Query query = QueryFactory.create(s1); 
-		QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
-		ResultSet results = qExe.execSelect();
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-		ResultSetFormatter.outputAsJSON(outStream, results);
-		String json = new String(outStream.toByteArray());
-		System.out.println(json);
+			Query query = QueryFactory.create(s1); 
+			QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
+			ResultSet results = qExe.execSelect();
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			ResultSetFormatter.outputAsJSON(outStream, results);
+			jsonData = new String(outStream.toByteArray());
+			System.out.println(jsonData);
+		} else if (requestType.equals("selectData")) {
+			System.out.println("selectData");
+			String input = request.getParameter("input");
+			System.out.println(input);
+			if (jsonData == null) {
+				//error
+			}
+			 //db insert action
+		} else if (requestType.equals("listData")) {
+			System.out.println("listData");
+			//db select action
+		} else {
+			System.out.println("Unexpected request type: " + requestType);
+		}
 	}
 
 	/**
