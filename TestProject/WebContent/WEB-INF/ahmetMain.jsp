ed26196e590371e6f43fd88403dac25ec206bf3f
ahmet zorer <ahmetcjr@hotmail.com>
2016-05-09 15:33:39 +0300

    1. json object is added to dependencies 2. checkbox is successfully implemented

---
+++ TestProject/WebContent/WEB-INF/ahmetMain.jsp
@@ -0,0 +1,133 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Query Analysis</title>
+
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<link
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+	rel="stylesheet" type="text/css">
+	
+<style type="text/css">
+#custom-search-input{
+    padding: 3px;
+    border: solid 1px #E4E4E4;
+    border-radius: 6px;
+    background-color: #fff;
+}
+#custom-search-input input{
+    border: 0;
+    box-shadow: none;
+}
+#custom-search-input button{
+    margin: 2px 0 0 0;
+    background: none;
+    box-shadow: none;
+    border: 0;
+    color: #666666;
+    padding: 0 8px 0 10px;
+    border-left: solid 1px #ccc;
+}
+#custom-search-input button:hover{
+    border: 0;
+    box-shadow: none;
+    border-left: solid 1px #ccc;
+}
+#custom-search-input .glyphicon-search{
+    font-size: 23px;
+}
+</style>
+
+<script type="text/javascript">
+	 
+	function submitSearch() {
+		$.get("", {
+			type : "queryData",
+			input : $("#queryInput").val()
+		}).done(function(dat) { //print the resulting data in table format
+			 var data = eval("(" + dat + ")");
+			 var columns = data["head"]["vars"];
+			 var myList = data["results"]["bindings"];
+		 
+			 $("#excelDataTable").append('<tr/>');
+			 var row$ = $('<tr/>').append($('<th/>').html("Select"));
+			 for (var i = 0; i < columns.length; ++i) {
+				 row$.append($('<th/>').html(columns[i]));
+			 }
+			 $("#excelDataTable").append(row$);
+			 for (var i = 0 ; i < myList.length ; i++) {
+				 row$ = $('<tr/>').append($('<td/>').html("<input type=\"checkbox\" name=\"id\" value="+i+">"));
+				 for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+					 var cellValue = myList[i][columns[colIndex]]["value"];
+					 if (cellValue == null) {
+						 cellValue = "";
+					 }
+					 row$.append($('<td/>').html(cellValue));
+				 }
+				 $("#excelDataTable").append(row$);
+		 	}
+		});
+	}
+	
+	function submitCheckbox() {
+		var string = [];
+		$('#excelDataTable').find('tr').each(function () {
+	        var row = $(this);
+	        if (row.find('input[type="checkbox"]').is(':checked')) {
+	            console.log(row);
+	        }
+	    });
+		$.get("", {
+			type : "selectData",
+			input : $(string).val()
+		}).done(function(data) {
+			//success or fail message
+		});
+	}
+	function listData() {
+		$.get("", {
+			type : "listData"
+		}).done(function(data) {
+			//print the data in a table without checkboxes
+		});
+	}
+</script>
+
+</head>
+
+<body>
+<div class="container">
+	<div class="row">
+        <div class="col-md-6">
+    		<h2>Search the Data</h2>
+            <div id="custom-search-input">
+            	<form onsubmit="submitSearch();return false" id="searchForm" role="form">
+                <div class="input-group col-md-12">
+                    <input id="queryInput"type="text" class="form-control input-lg" name="queryInput" placeholder="Enter Query Term..." />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+                </form>
+            </div>
+            
+            <form action="">
+			<table id="excelDataTable" border="1"> </table>
+			<button onclick="submitCheckbox()" >
+				<i class="glyphicon glyphicon-save"></i>
+            </button>
+			</form>
+        </div>
+	</div>
+</div>
+
+</body>
+</html>
--- TestProject/src/AhmetServlet.java
+++ TestProject/src/AhmetServlet.java
@@ -1,60 +1,93 @@
-
-
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+
 /**
- * Servlet implementation class AhmetServlet
+ * Servlet implementation class EsrefServlet
  */
 @WebServlet("/ahmet_zorer")
 public class AhmetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AhmetServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static String jsonData = null;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AhmetServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		String query = "SELECT ?item ?itemLabel ?pic WHERE { ?item ?x1 wd:Q146 . OPTIONAL { ?item wdt:P18 ?pic } SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\" } }";
-        String website = "https://query.wikidata.org/sparql?query=";
-		try {
-			String queryResult = getHTML("https://query.wikidata.org/sparql?query=SELECT%20?h%20WHERE{?h%20wdt:P31%20wd:Q3624078}");
-			out.println(queryResult);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		String requestType = request.getParameter("type");
+		if (requestType == null) {
+			request.getRequestDispatcher("/WEB-INF/ahmetMain.jsp").forward(request, response);;
+		} else if (requestType.equals("queryData")) {
+			String inputTerm = request.getParameter("input");
+			String s1 = 
+					"PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
+					"PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
+					"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
+					"PREFIX wd: <http://www.wikidata.org/entity/>\n" +
+					"select ?objectLabel ?countryLabel ?coord\n" +
+					"where {\n" +
+					"?object wdt:P31 wd:Q46169 . \n" +
+					"?object wdt:P625 ?coord .\n" +
+					"?object wdt:P17 ?country .\n" +
+					"SERVICE wikibase:label {\n" +
+					"bd:serviceParam wikibase:language \"en\" .\n" +
+					"}\n" +
+					"}" +
+					"LIMIT 100\n";
+			Query query = QueryFactory.create(s1); 
+			QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
+			ResultSet results = qExe.execSelect();
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			ResultSetFormatter.outputAsJSON(outStream, results);
+			jsonData = new String(outStream.toByteArray());
+			response.getWriter().write(jsonData);
+//			System.out.println(jsonData);
+		} else if (requestType.equals("selectData")) {
+			System.out.println("selectData");
+			String input = request.getParameter("input"); //TODO: must be a list
+			System.out.println(input);
+			if (jsonData == null) {
+				//error
+			}
+			 //db insert action
+			if (true/*success*/) {
+				response.getWriter().write(1);
+			} else {
+				response.getWriter().write(0);
+			}
+		} else if (requestType.equals("listData")) {
+			System.out.println("listData");
+			//db select action
+			String sqlData = "SQLDATA"; //all the data in mySQL server
+			response.getWriter().write(sqlData);
+		} else {
+			System.out.println("Unexpected request type: " + requestType);
 		}
 	}
-	
-	public static String getHTML(String urlToRead) throws Exception {
-		 URL oracle = new URL(urlToRead);
-	        BufferedReader in = new BufferedReader(
-	        new InputStreamReader(oracle.openStream()));
 
-	        String sum,inputLine;
-	        while ((inputLine = in.readLine()) != null)
-	            System.out.println(inputLine);
-	        in.close();
-	      return inputLine;
-	   }
-
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
 }
