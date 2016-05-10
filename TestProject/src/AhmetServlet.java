3daa32957584495f7481b05827c348aec005812e
ahmet zorer <ahmetcjr@hotmail.com>
2016-05-10 21:07:50 +0300

    sone Updates

--- TestProject/src/AhmetServlet.java
+++ TestProject/src/AhmetServlet.java
@@ -1,5 +1,9 @@
-import java.io.ByteArrayOutputStream;
+import java.awt.Point;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,26 +14,259 @@ import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
-
 /**
- * Servlet implementation class EsrefServlet
+ * Servlet implementation class AhmetServlet
  */
 @WebServlet("/ahmet_zorer")
 public class AhmetServlet extends HttpServlet {
+	public class University {
+		String name;
+		int date;
+		double longtitude;
+		double latitude;
+
+		public University(String name, String date, String coord) {
+			this.name = name;
+			this.date = Integer.parseInt(date);
+			
+			this.longtitude = Double.parseDouble(coord.substring(0,coord.indexOf(" ")));
+			this.latitude = Double.parseDouble(coord.substring(coord.indexOf(" ")+1));		
+		}
+	}
+
 	private static final long serialVersionUID = 1L;
-	private static String jsonData = null;
+	private static ArrayList<University> parks = null;
+	private static String url = "jdbc:mysql://ec2-54-186-213-92.us-west-2.compute.amazonaws.com:3306/db";
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public AhmetServlet() {
 		super();
-		// TODO Auto-generated constructor stub
+	}
+	
+	/** Computes the number of steps it will take to get to the second string from the first one
+	 * by computing the levenshtein distance between the two strings.
+	 * 
+	 * @param lhs Starting string
+	 * @param rhs Target string
+	 * @return Number of steps it will take to get to the second string from the first one.
+	 */
+	public static int levenshteinDistance(CharSequence lhs, CharSequence rhs) {      
+		int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];        
+		for (int i = 0; i <= lhs.length(); i++) {
+			distance[i][0] = i;
+		}
+		for (int j = 1; j <= rhs.length(); j++) {
+			distance[0][j] = j;
+		}
+		for (int i = 1; i <= lhs.length(); i++) {
+			for (int j = 1; j <= rhs.length(); j++) {
+				int first = Math.min(distance[i-1][j]+1, distance[i][j-1]+1);
+				int second = distance[i-1][j-1] + ((lhs.charAt(i-1) == rhs.charAt(j-1)) ? 0 : 1);
+				distance[i][j] = Math.min(first, second);
+			}
+		}
+		return distance[lhs.length()][rhs.length()];                           
+	}
+	
+	/** Queries data from wikidata and returns the resulting data as a String in an internal data format.
+	 * 
+	 * Internal data format is as follows:
+	 * ROW&&ROW&&ROW&&...&&ROW
+	 * 
+	 * Each ROW is formatted as follows:
+	 * COLUMN||COLUMN||...||COLUMN
+	 * 
+	 * @param request HTTPServletRequest object containing the request parameters input and type.
+	 * @return Resulting data in an internal data format.
+	 */
+	private String queryData(HttpServletRequest request) {
+		parks = new ArrayList<University>();
+		String queryString ="PREFIX wikibase: <http://wikiba.se/ontology#>"
+				+ "PREFIX bd: <http://www.bigdata.com/rdf#>"
+				+ "PREFIX wd: <http://www.wikidata.org/entity/>"
+				+ "PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
+				+ "select ?universityLabel ?universityDescription ?date ?coord WHERE {"
+				+ "	?university wdt:P31/wdt:P279* wd:Q3918 ;"
+				+ "		wdt:P17 wd:Q43 ;"
+				+ "		wdt:P625 ?coord;"
+				+ "        wdt:P571 ?date."
+				+ "	OPTIONAL {"
+				+ "		?university wdt:P856 ?website"
+				+ "	}"
+				+ "	SERVICE wikibase:label {"
+				+ "		bd:serviceParam wikibase:language \"en, tr\" ."
+				+ "	}"
+				+ "}";
+		Query query = QueryFactory.create(queryString); 
+		QueryExecution qExe = QueryExecutionFactory.sparqlService("https://query.wikidata.org/sparql", query);
+		ResultSet results = qExe.execSelect();
+
+		while (results.hasNext()) {
+			QuerySolution next = results.nextSolution();
+			String name = next.get("?universityLabel").toString();
+			String date = next.get("?date").toString().substring(0,4);
+			String coord = next.get("?coord").toString();
+			if (name.contains("@")) {
+				name = name.substring(0, name.indexOf('@'));
+			}
+			coord = coord.substring(6, coord.indexOf(")"));
+			System.out.println(name+", "+date+", "+coord);
+			parks.add(new University(name, date, coord));
+
+		}
+		
+		
+		
+		//TODO
+		
+		
+		/*this.parseData(results);
+		this.semanticRanking(request.getParameter("input"));
+		StringBuilder data = new StringBuilder("Name||Country||Longtitude||Latitude&&");
+		for(NationalPark np : parks) {
+			data.append(np.name + "||" + np.country + "||" + np.longtitude + "||" + np.latitude + "&&");
+		}*/
+		return "";
+	}
+	
+	/** Inserts the records in the specified indices to MySQL server.
+	 * Which records are going to be inserted is specified in "input" parameter of GET call.
+	 * "input" parameter is a space separated string with each entry specifying the index of
+	 * the record to be inserted.
+	 * 
+	 * @param request HTTPServletRequest object containing the request parameters input and type.
+	 * @return Resulting data as a String.
+	 */
+	private String insertData(HttpServletRequest request) {
+		String filter = request.getParameter("input");
+		if (filter.equals("undefined")) {
+			return "0";
+		}
+		String[] idStrings = filter.split(" ");
+		ArrayList<University> universityList = new ArrayList<University>();
+		for (String idStr : idStrings) {
+			universityList.add(parks.get(Integer.parseInt(idStr)));
+		}
+		java.sql.Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection (url,"root","pembePanter");
+			java.sql.Statement stmt = connection.createStatement();
+			StringBuilder sqlStmt = new StringBuilder("INSERT INTO db.University VALUES");
+			for (int i = 0; i < universityList.size() - 1; ++i) {
+				University np = universityList.get(i);
+				sqlStmt.append("(\""); 
+				sqlStmt.append(np.name); 
+				sqlStmt.append("\", \""); 
+				sqlStmt.append(np.date); 
+				sqlStmt.append("\", "); 
+				sqlStmt.append(np.longtitude); 
+				sqlStmt.append(", "); 
+				sqlStmt.append(np.latitude); 
+				sqlStmt.append("), ");
+			}
+			University last = universityList.get(universityList.size() - 1);
+			sqlStmt.append("(\""); 
+			sqlStmt.append(last.name); 
+			sqlStmt.append("\", \""); 
+			sqlStmt.append(last.date); 
+			sqlStmt.append("\", "); 
+			sqlStmt.append(last.longtitude); 
+			sqlStmt.append(", "); 
+			sqlStmt.append(last.latitude); 
+			sqlStmt.append(");");
+			stmt.executeUpdate(sqlStmt.toString());
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "0";
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return "0";
+		}
+		return "1";
+	}
+	
+
+	
+	private String deleteData(HttpServletRequest request) {
+		String filter = request.getParameter("input");
+		if (filter.equals("undefined")) {
+			return "0";
+		}
+		ArrayList<University> checkedNPs = getSelectedParks(filter);
+		java.sql.Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection (url,"root","pembePanter");
+			java.sql.Statement stmt = connection.createStatement();
+			for (University np : checkedNPs) {
+				String sqlStmt = "DELETE FROM db.NationalPark WHERE Name = \"" + np.name + "\";";
+				stmt.executeUpdate(sqlStmt);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "0";
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return "0";
+		}			
+		return "1";
+	}
+	
+	/** Gets all the records stored in MySQL server and returns them as a String.
+	 * 
+	 * @param request HTTPServletRequest object contaning the request parameters input and type.
+	 * @return Resulting data as a String.
+	 */
+	private String listData(HttpServletRequest request) {
+		java.sql.Connection connection;
+		StringBuilder data = new StringBuilder("");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection (url,"root","pembePanter");
+			java.sql.Statement stmt = connection.createStatement();
+			String sqlStmt = "SELECT * FROM db.NationalPark;";
+			java.sql.ResultSet res = stmt.executeQuery(sqlStmt);
+			while (res.next()) {
+				data.append(res.getString("Name"));
+				data.append("||");
+				data.append(res.getString("Country"));
+				data.append("||");
+				data.append(res.getDouble("Longtitude"));
+				data.append("||");
+				data.append(res.getDouble("Latitude"));
+				data.append("&&");
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "0";
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return "0";
+		}			
+		return data.toString();
 	}
 
-	/**
+	/** doGet method responding to GET calls.
+	 * According to different request types, executes corresponding processes and returns the resulting data.
+	 * 
+	 * Accepts two parameters from the GET method:
+	 * 1. type : Request type. May be
+	 * 	i.   queryData
+	 *  ii.  insertData
+	 *  iii. listData
+	 * 2. input : Input data of a request. Each request type requires different input:
+	 * queryData  : A search term such as name of a national park.
+	 * insertData : List of checkbox id's as a string.
+	 * listData   : No input
+	 * 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,54 +274,20 @@ public class AhmetServlet extends HttpServlet {
 		if (requestType == null) {
 			request.getRequestDispatcher("/WEB-INF/ahmetMain.jsp").forward(request, response);;
 		} else if (requestType.equals("queryData")) {
-			String inputTerm = request.getParameter("input");
-			String s1 = 
-					"PREFIX wikibase: <http://wikiba.se/ontology#>\n" +
-					"PREFIX bd: <http://www.bigdata.com/rdf#>\n" +
-					"PREFIX wdt: <http://www.wikidata.org/prop/direct/>\n" +
-					"PREFIX wd: <http://www.wikidata.org/entity/>\n" +
-					"select ?objectLabel ?countryLabel ?coord\n" +
-					"where {\n" +
-					"?object wdt:P31 wd:Q46169 . \n" +
-					"?object wdt:P625 ?coord .\n" +
-					"?object wdt:P17 ?country .\n" +
-					"SERVICE wikibase:label {\n" +
-					"bd:serviceParam wikibase:language \"en\" .\n" +
-					"}\n" +
-					"}" +
-					"LIMIT 100\n";
-			Query query = QueryFactory.create(s1); 
-			QueryExecution qExe = QueryExecutionFactory.sparqlService( "https://query.wikidata.org/sparql", query );
-			ResultSet results = qExe.execSelect();
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			ResultSetFormatter.outputAsJSON(outStream, results);
-			jsonData = new String(outStream.toByteArray());
-			response.getWriter().write(jsonData);
-//			System.out.println(jsonData);
-		} else if (requestType.equals("selectData")) {
-			System.out.println("selectData");
-			String input = request.getParameter("input"); //TODO: must be a list
-			System.out.println(input);
-			if (jsonData == null) {
-				//error
-			}
-			 //db insert action
-			if (true/*success*/) {
-				response.getWriter().write(1);
-			} else {
-				response.getWriter().write(0);
-			}
+			response.getWriter().write(queryData(request));
+		} else if (requestType.equals("insertData")) {
+			response.getWriter().write(insertData(request));
+		} else if (requestType.equals("deleteData")) {
+			response.getWriter().write(deleteData(request));
 		} else if (requestType.equals("listData")) {
-			System.out.println("listData");
-			//db select action
-			String sqlData = "SQLDATA"; //all the data in mySQL server
-			response.getWriter().write(sqlData);
+			response.getWriter().write(listData(request));
 		} else {
 			System.out.println("Unexpected request type: " + requestType);
 		}
 	}
 
-	/**
+	/** doPost method responding to POST calls.
+	 * Currently not used.
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
