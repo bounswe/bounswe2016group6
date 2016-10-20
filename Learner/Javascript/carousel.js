2ee7937406b7e3d9143d1adbd5972bc61c008b86
Ali Erkilic <erkilic.alican@gmail.com>
2016-10-20 22:13:45 +0300

    added registered user home page files.

---
+++ Learner/Javascript/carousel.js
@@ -0,0 +1,7 @@
+$('.carousel-control.left').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control.right').click(function() {
+  $('#myCarousel').carousel('next');
+});
