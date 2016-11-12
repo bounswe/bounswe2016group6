e0749d8febe0872c887d31ecaa97aa6c265ee97a
Ahmet Zorer <ahmetcj4@hotmail.com>
2016-11-12 13:57:06 +0300

    -sample commens are added

---
+++ Android/Learner/app/src/main/java/com/group6boun451/learner/model/Topic.java
@@ -0,0 +1,149 @@
+package com.group6boun451.learner.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Topic {
+  private Long id;
+    
+    private String header;
+    
+    private String content;
+
+    private User owner;
+    
+    private int likes;
+    
+    private List<User> likedBy;
+    private List<Tag> tags;
+    private List<Comment> comments;
+
+    private String headerImage;
+    private Date createdAt;
+    private Date revealDate;
+
+    public Topic() {
+        super();
+    }
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getRevealDate() {
+		return revealDate;
+	}
+
+	public void setRevealDate(Date revealDate) {
+		this.revealDate = revealDate;
+	}
+
+    public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	@Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((header == null) ? 0 : header.hashCode());
+        result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Topic other = (Topic) obj;
+
+        return header.equals(other.header);
+
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Topic :").append(header);
+        return builder.toString();
+    }
+
+    public List<User> getLikedBy() {
+        return likedBy;
+    }
+
+    public void setLikedBy(List<User> likedBy) {
+        this.likedBy = likedBy;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public String getHeaderImage() {
+        return headerImage;
+    }
+
+    public void setHeaderImage(String headerImage) {
+        this.headerImage = headerImage;
+    }
+}
