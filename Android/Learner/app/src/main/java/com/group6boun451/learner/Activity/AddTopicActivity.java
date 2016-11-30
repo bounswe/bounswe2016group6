ef8f1caed9001d0d37f81c8f7c0fa4c86df99595
Ahmet Zorer <ahmetcj4@hotmail.com>
2016-11-30 15:36:05 +0300

    tag view is added

--- Android/Learner/app/src/main/java/com/group6boun451/learner/Activity/AddTopicActivity.java
+++ Android/Learner/app/src/main/java/com/group6boun451/learner/Activity/AddTopicActivity.java
@@ -13,17 +13,24 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TabHost;
+import android.widget.TextView;
 
+import com.doodle.android.chips.ChipsView;
+import com.doodle.android.chips.model.Contact;
 import com.group6boun451.learner.R;
 import com.group6boun451.learner.model.GenericResponse;
 import com.group6boun451.learner.model.Topic;
@@ -46,7 +53,10 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -55,6 +65,10 @@ public class AddTopicActivity extends AppCompatActivity {//implements DatePicker
     protected static final String TAG = HomePage.class.getSimpleName();
     private static final int PICK_IMAGE = 2;
     private static final int EDITOR = 3;
+    private RecyclerView mContacts;
+    private ContactsAdapter mAdapter;
+    private ChipsView mChipsView;
+
     @BindView(R.id.summernote) Summernote summernote;
     @BindView(R.id.content_hamburger) View contentHamburger;
     @BindView(R.id.toolbar) Toolbar toolbar;
@@ -86,7 +100,8 @@ public class AddTopicActivity extends AppCompatActivity {//implements DatePicker
 
         tabHost.setup();
         tabHost.addTab(tabHost.newTabSpec("Tab One").setContent(R.id.topic_layout).setIndicator("Topic"));
-        tabHost.addTab(tabHost.newTabSpec("Tab Two").setContent(R.id.editor_layout).setIndicator("Content"));
+        tabHost.addTab(tabHost.newTabSpec("Tab Two").setContent(R.id.tag_layout).setIndicator("Tags"));
+        tabHost.addTab(tabHost.newTabSpec("Tab Three").setContent(R.id.editor_layout).setIndicator("Content"));
 
 
         topicNameEditText.addTextChangedListener(new TextWatcher() {
@@ -138,6 +153,49 @@ public class AddTopicActivity extends AppCompatActivity {//implements DatePicker
                 })
                 .setClosedOnStart(true)
                 .build();
+
+
+
+        mContacts = (RecyclerView) findViewById(R.id.rv_contacts);
+        mContacts.setLayoutManager(new LinearLayoutManager(AddTopicActivity.this));
+        mAdapter = new ContactsAdapter();
+        mContacts.setAdapter(mAdapter);
+
+        mChipsView = (ChipsView) findViewById(R.id.cv_contacts);
+
+        // change EditText config
+        mChipsView.getEditText().setCursorVisible(true);
+
+        mChipsView.setChipsValidator(new ChipsView.ChipValidator() {
+            @Override
+            public boolean isValid(Contact contact) {
+                return !contact.getDisplayName().equals("asd@qwe.de");
+            }
+        });
+
+        mChipsView.setChipsListener(new ChipsView.ChipsListener() {
+            @Override
+            public void onChipAdded(ChipsView.Chip chip) {
+                for (ChipsView.Chip chipItem : mChipsView.getChips()) {
+                    Log.d("ChipList", "chip: " + chipItem.toString());
+                }
+            }
+
+            @Override
+            public void onChipDeleted(ChipsView.Chip chip) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text) {
+                mAdapter.filterItems(text);
+            }
+        });
+
+
+
+
+
         summernote.setRequestCodeforFilepicker(EDITOR);
         date = new int[6];
 //        setDatePicker();
@@ -265,6 +323,98 @@ public class AddTopicActivity extends AppCompatActivity {//implements DatePicker
             //new AddContentTask().execute();
         }
     }
+
+    public class ContactsAdapter extends RecyclerView.Adapter<CheckableContactViewHolder> {
+
+        private String[] data = new String[]{
+                "john@doe.com",
+                "at@doodle.com",
+                "asd@qwe.de",
+                "verylongaddress@verylongserver.com",
+                "thisIsMyEmail@address.com",
+                "test@testeration.de",
+                "short@short.com"
+        };
+
+        private List<String> filteredList = new ArrayList<>();
+
+        public ContactsAdapter() {
+            Collections.addAll(filteredList, data);
+        }
+
+        @Override
+        public CheckableContactViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(AddTopicActivity.this).inflate(R.layout.item_checkable_contact, parent, false);
+            return new CheckableContactViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(CheckableContactViewHolder holder, int position) {
+            holder.name.setText(filteredList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return filteredList.size();
+        }
+
+        public void filterItems(CharSequence text) {
+            filteredList.clear();
+            if (TextUtils.isEmpty(text)) {
+                Collections.addAll(filteredList, data);
+            } else {
+                for (String s : data) {
+                    if (s.contains(text)) {
+                        filteredList.add(s);
+                    }
+                }
+            }
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return Math.abs(filteredList.get(position).hashCode());
+        }
+    }
+
+    public class CheckableContactViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView name;
+        public final CheckBox selection;
+
+        public CheckableContactViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.tv_contact_name);
+            selection = (CheckBox) itemView.findViewById(R.id.cb_contact_selection);
+            selection.setOnClickListener(this);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selection.performClick();
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+            String email = name.getText().toString();
+            Uri imgUrl = Math.random() > .7d ? null : Uri.parse("https://robohash.org/" + Math.abs(email.hashCode()));
+            Contact contact = new Contact(null, null, null, email, imgUrl);
+
+            if (selection.isChecked()) {
+                mChipsView.addChip(email, imgUrl, contact);
+            } else {
+                mChipsView.removeChipBy(contact);
+            }
+        }
+    }
+
+
+
+
+
+
 
     // ***************************************
     // Private methods
