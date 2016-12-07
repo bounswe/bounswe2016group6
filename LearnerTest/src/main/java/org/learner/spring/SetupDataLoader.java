820dc7b546ba0594f86e85515d870ad4de8334b6
Erhan Çağırıcı <erhan.cagirici@boun.edu.tr>
2016-12-07 20:41:27 +0200

    Persistence settings updated.

--- LearnerTest/src/main/java/org/learner/spring/SetupDataLoader.java
+++ LearnerTest/src/main/java/org/learner/spring/SetupDataLoader.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
-    private boolean alreadySetup = false;
+    private boolean alreadySetup = true;
 
     @Autowired
     private UserRepository userRepository;
